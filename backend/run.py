3e15e9d577f76c013c2f76063f7bf640762337f6
Prajith Reddy <gurramprajith@gmail.com>
2025-03-11 11:37:49 +0530

    vercel changes

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,5 @@
-import uvicorn
-from dotenv import load_dotenv
+from main import app
 
-if __name__ == "__main__":
-    load_dotenv()
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# Remove the uvicorn run part as Vercel will handle that
+from dotenv import load_dotenv
+load_dotenv()
